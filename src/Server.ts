c19ea8785c39a895ae7650999c42f3094d256677
thuantq1 <thuantq1@vnext.com.vn>
2021-03-31 18:02:25 +0700

    Add cors module

--- src/Server.ts
+++ src/Server.ts
@@ -3,7 +3,7 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import StatusCodes from 'http-status-codes'
 import express, { NextFunction, Request, Response } from 'express'
-
+import cors from 'cors'
 import 'express-async-errors'
 
 import BaseRouter from './routes'
@@ -17,12 +17,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser(cookieProps.secret))
 
-// Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
+  app.use(cors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000'
+    ]
+  }))
 }
 
-// Security
 if (process.env.NODE_ENV === 'production') {
   app.use(helmet())
 }
