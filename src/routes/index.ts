c5eef4372dfd20ac540bc8d92eba2c6938aac506
thuantq1 <thuantq1@vnext.com.vn>
2021-04-14 15:02:17 +0700

    Optimize router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,9 @@ import { adminMW } from '@routes/middleware'
 import AuthRouter from '@routes/Auth'
 import UserRouter from '@routes/Users'
 import S3Router from '@routes/S3'
-import swaggerUi from 'swagger-ui-express'
-import * as swaggerDocument from './../swagger.json'
 
 const router = Router()
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 router.use('/auth', AuthRouter)
 router.use('/users', adminMW, UserRouter)
 router.use('/s3', adminMW, S3Router)
