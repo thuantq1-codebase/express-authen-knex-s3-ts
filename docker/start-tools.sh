eaa62f818689e71ce2947629fc31f63eb6cbd471
thuantq1 <thuantq1@vnext.com.vn>
2021-04-14 14:11:44 +0700

    Optimize docker and README

--- docker/start-tools.sh
+++ docker/start-tools.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-docker-compose \
+docker-compose --env-file ../src/pre-start/env/development.env \
   -f docker-compose.local.yml \
   -f docker-compose.localdb.yml \
   -f docker-compose.localstack.yml \
